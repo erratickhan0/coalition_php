e06f9bbaeb6ca80c1f749544866b746edcab543c
Zeeshan khan <zeeshankhan.od@gmail.com>
2019-02-18 21:17:01 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Adnan
+ * Date: 2/18/19
+ * Time: 9:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo 'hello world';
